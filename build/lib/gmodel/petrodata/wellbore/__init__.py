c00adf5cede115e4bcb3c4a50a950c6eefe83930
Javid Shiriyev, Ph.D. <jshiriyev@utexas.edu>
2025-04-04 18:42:10 +0400

    .

--- build/lib/gmodel/petrodata/wellbore/__init__.py
+++
@@ -1,9 +0,0 @@
-from ._well import Name, Slot, Well
-
-from ._drilling import Target, Drilling
-
-from ._layout import Pipe, Layout
-from ._survey import Survey, Depth
-
-from ._zones import Zones
-from ._perfs import Perf, Perfs
--- build/lib/gmodel/spstats/__init__.py
+++
@@ -1,7 +0,0 @@
-# from ._smeasures import DykstraParson
-# from ._smeasures import LorenzCoeff
-
-# from ._preprocess import Decluster
-# from ._variogram import Variogram
-# from ._kriging import Kriging
-# from ._simulation import Simulation
